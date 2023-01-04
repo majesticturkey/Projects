00831337e3c665597679f03109d0bf2312dcbeee
A Majestic Turkey <majesticturkey@gmail.com>
2023-01-04 00:41:16 -0500

    Let's get cooking on the minions API

--- Servers/BossMachine/server/api.js
+++ Servers/BossMachine/server/api.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const app = require('../server');
 const apiRouter = express.Router();
+const db = require('./db.js');
 
-
+app.get('/minions/', (req, res, next) => {
+    res.send(db.findDataArrayByName(minions));
+    next();
+});
 
 module.exports = apiRouter;
